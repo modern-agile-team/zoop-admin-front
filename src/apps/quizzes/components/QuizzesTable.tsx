5f79ddca86fef5c7125968d6fe731adbe2ba6989
zzzryt <jinjinstar3@gmail.com>
2025-10-15 10:27:56 +0900

    feat: 시트에 열 추가하기 #27

--- src/apps/quizzes/components/QuizzesTable.tsx
+++ src/apps/quizzes/components/QuizzesTable.tsx
@@ -1,10 +1,28 @@
-import { Table } from 'antd';
+import { Select, Space, Table } from 'antd';
 import Column from 'antd/es/table/Column';
 import ColumnGroup from 'antd/es/table/ColumnGroup';
+import { useState } from 'react';
 
 import type { Quiz } from '../mock/quizzes';
 import { QUIZZES_MOCK_DATA } from '../mock/quizzes';
 export default function QuizzesTable() {
+  const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES_MOCK_DATA);
+
+  const addQuizTableHandler = () => {
+    const newData: Quiz = {
+      id: '',
+      question: '',
+      answer: '',
+      imageUrl: '',
+      type: 'multipleChoice',
+      createdAt: '',
+      updatedAt: '',
+    };
+    setQuizzes((prev) => {
+      return [...prev, newData];
+    });
+  };
+
   return (
     <>
       <header className="p-6 border-b border-contents-200 flex justify-between items-center">
@@ -21,8 +39,31 @@ export default function QuizzesTable() {
 
       <main className="p-6">
         <section>
-          <Table<Quiz> dataSource={QUIZZES_MOCK_DATA}>
-            <Column title="ID" dataIndex="id" key="id" />
+          <Table<Quiz> dataSource={quizzes}>
+            <Column
+              title="타입"
+              dataIndex="type"
+              key="type"
+              render={(type) => (
+                <Space wrap>
+                  <Select
+                    defaultValue={type}
+                    style={{ width: 120 }}
+                    onChange={addQuizTableHandler}
+                    options={[
+                      { value: 'multipleChoice', label: '다중선택' },
+                      { value: 'shortAnswer', label: '짧은 글' },
+                      { value: 'selectPicture', label: '그림맞추기' },
+                      {
+                        value: 'correctWords',
+                        label: '단어맞추기',
+                        disabled: true,
+                      },
+                    ]}
+                  />
+                </Space>
+              )}
+            />
             <Column title="질문" dataIndex="question" key="question" />
             <Column title="정답" dataIndex="answer" key="answer" />
             <Column
@@ -30,7 +71,7 @@ export default function QuizzesTable() {
               dataIndex="imageUrl"
               key="imageUrl"
               render={(image) => {
-                if (image === null) return null;
+                if (!image) return null;
                 return (
                   <>
                     <img src={image} alt="image" width={50} height={50} />
@@ -44,7 +85,10 @@ export default function QuizzesTable() {
             </ColumnGroup>
           </Table>
         </section>
-        <button className="bg-primary-600 px-2 py-1 mt-3 text-white rounded-lg hover:bg-primary-700 transition-colors cursor-pointer">
+        <button
+          onClick={addQuizTableHandler}
+          className="bg-primary-600 px-2 py-1 mt-3 text-white rounded-lg hover:bg-primary-700 transition-colors cursor-pointer"
+        >
           +
         </button>
       </main>
