5829e8594cdbe91c729f450eb7cc67fc5e0fe779
soonki-98 <alstnsrl98@gmail.com>
2025-10-15 21:17:42 +0900

    feat: 로그인을 한 상태로 로그인 페이지에 접근하면 redirectUrl로 이동  #75

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,7 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import z from 'zod';
 
 import LoginPage from '@/apps/login';
+import { STORAGE } from '@/shared/utils/storage';
 
 const urlSearchSchema = z.object({
   redirectUrl: z.string(),
@@ -9,6 +10,17 @@ const urlSearchSchema = z.object({
 
 export const Route = createFileRoute('/(auth)/login/')({
   component: LoginPage,
+  loader: (ctx) => {
+    const result = urlSearchSchema.safeParse(ctx.location.search);
+
+    if (!result.success) {
+      throw redirect({ to: '/login', search: { redirectUrl: '/' } });
+    }
+
+    if (STORAGE.getAuthToken()) {
+      throw redirect({ to: result.data.redirectUrl });
+    }
+  },
   validateSearch: (search) => {
     const result = urlSearchSchema.safeParse(search);
     if (!result.success) {
