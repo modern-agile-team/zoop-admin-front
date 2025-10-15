d103e25316363c21cef590f015e0c6a4b0bd0e0f
soonki-98 <alstnsrl98@gmail.com>
2025-10-15 17:53:58 +0900

    feat: 이미지 관리 페이지에서 api 호출 #72

--- src/apps/assets/images/index.tsx
+++ src/apps/assets/images/index.tsx
@@ -1,3 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { Typography } from 'antd';
+
+import { imageQueries } from '@/shared/service/query/image';
+
 export default function ImageAssetPage() {
-  return <div>Hello "/assets/images/"!</div>;
+  const { data } = useQuery(imageQueries.getList({}));
+
+  return (
+    <div>
+      <Typography.Title level={1}>이미지 관리</Typography.Title>
+      {data?.data.map((image) => (
+        <div key={image.id}>
+          <img src={image.imageUrl} alt={image.id} width={100} height={100} />
+        </div>
+      ))}
+    </div>
+  );
 }
