12a3bca8ef6e8771623f328cf53b91054a5fd16f
soonki-98 <alstnsrl98@gmail.com>
2025-10-17 23:12:15 +0900

    feat: 페이지네이션을 위한 search param validation 추가 #95

--- src/routes/(menus)/assets/images/index.tsx
+++ src/routes/(menus)/assets/images/index.tsx
@@ -1,7 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router';
+import z from 'zod';
 
 import ImageAssetPage from '@/apps/assets/images';
 
+const searchParamsSchema = z.object({
+  page: z.number().min(1).default(1),
+});
+
 export const Route = createFileRoute('/(menus)/assets/images/')({
   component: ImageAssetPage,
+  validateSearch: (search) => {
+    const result = searchParamsSchema.safeParse(search);
+    if (!result.success) {
+      return { page: 1 };
+    }
+    return result.data;
+  },
 });
