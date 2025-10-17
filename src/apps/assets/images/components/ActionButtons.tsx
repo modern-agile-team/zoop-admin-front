51c45ef005847302caf2f545f9f6056166cb07f8
soonki-98 <alstnsrl98@gmail.com>
2025-10-17 22:36:25 +0900

    feat: overlay-kit 제거 및 antd app provider 등록 #92

--- src/apps/assets/images/components/ActionButtons.tsx
+++ src/apps/assets/images/components/ActionButtons.tsx
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Alert, Button, message, Modal, Upload } from 'antd';
-import { overlay } from 'overlay-kit';
+import { Alert, App, Button, Upload } from 'antd';
 
 import { imageQueries } from '@/shared/service/query/image';
 
@@ -14,18 +13,18 @@ export default function ActionButtons({
   onRemoveImages,
 }: Props) {
   const queryClient = useQueryClient();
-  const [messageApi, contextHolder] = message.useMessage();
+  const { message, modal } = App.useApp();
 
   const { mutate: uploadImage, isPending } = useMutation({
     ...imageQueries.uploadImage,
     onSuccess: () => {
-      messageApi.success('이미지를 업로드했어요.');
+      message.success('이미지를 업로드했어요.');
       queryClient.invalidateQueries({
         queryKey: imageQueries.getList({ category: undefined }).queryKey,
       });
     },
     onError: () => {
-      messageApi.error('업로드 중 오류가 발생했어요.');
+      message.error('업로드 중 오류가 발생했어요.');
     },
     retry: false,
   });
@@ -36,58 +35,52 @@ export default function ActionButtons({
   });
 
   const handleRemoveImages = async () => {
-    const shouldRemove = await overlay.openAsync<boolean>(
-      ({ isOpen, close }) => (
-        <Modal
-          title="이미지 삭제"
-          open={isOpen}
-          onCancel={() => close(false)}
-          onOk={() => close(true)}
-          okButtonProps={{ danger: true }}
-        >
-          <Alert
-            message="선택한 이미지를 삭제하시겠습니까?"
-            description="삭제된 이미지는 복구할 수 없습니다."
-            type="error"
-          />
-        </Modal>
-      )
-    );
+    const shouldRemove = await modal.confirm({
+      title: '이미지 삭제',
+      okText: '삭제',
+      cancelText: '취소',
+      okButtonProps: { danger: true },
+      icon: null,
+      content: (
+        <Alert
+          message="선택한 이미지를 삭제하시겠습니까?"
+          description="삭제된 이미지는 복구할 수 없습니다."
+          type="error"
+        />
+      ),
+    });
 
     if (shouldRemove) {
       try {
         await Promise.all(
           selectedImageIds.map((id) => removeImage(id.toString()))
         );
-        messageApi.success('이미지를 삭제했어요.');
+        message.success('이미지를 삭제했어요.');
       } catch {
-        messageApi.error('삭제 중 오류가 발생했어요.');
+        message.error('삭제 중 오류가 발생했어요.');
       }
       onRemoveImages();
     }
   };
 
   return (
-    <>
-      {contextHolder}
-      <div className="flex gap-4 self-end">
-        {selectedImageIds.length > 0 && (
-          <Button danger onClick={handleRemoveImages}>
-            선택된 이미지 삭제
-          </Button>
-        )}
-        <Upload
-          disabled={isPending}
-          multiple
-          accept="image/*"
-          customRequest={(args) => {
-            const { file } = args;
-            uploadImage({ file: file as File, category: '테스트' });
-          }}
-        >
-          <Button>이미지 업로드</Button>
-        </Upload>
-      </div>
-    </>
+    <div className="flex gap-4 self-end">
+      {selectedImageIds.length > 0 && (
+        <Button danger onClick={handleRemoveImages}>
+          선택된 이미지 삭제
+        </Button>
+      )}
+      <Upload
+        disabled={isPending}
+        multiple
+        accept="image/*"
+        customRequest={(args) => {
+          const { file } = args;
+          uploadImage({ file: file as File, category: '테스트' });
+        }}
+      >
+        <Button>이미지 업로드</Button>
+      </Upload>
+    </div>
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,17 @@
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
-import {
-  Outlet,
-  createRootRoute,
-  redirect,
-  useLocation,
-} from '@tanstack/react-router';
-import { OverlayProvider } from 'overlay-kit';
+import { Outlet, createRootRoute, redirect } from '@tanstack/react-router';
+import { App as AntdAppProvider } from 'antd';
 
 import { queryClient } from '@/lib/queryClient';
 import { STORAGE } from '@/shared/utils/storage';
 
 const Component = () => {
-  const location = useLocation();
-
   return (
     <QueryClientProvider client={queryClient}>
-      <OverlayProvider key={location.url}>
+      <AntdAppProvider>
         <Outlet />
-      </OverlayProvider>
+      </AntdAppProvider>
       <TanStackDevtools />
     </QueryClientProvider>
   );
