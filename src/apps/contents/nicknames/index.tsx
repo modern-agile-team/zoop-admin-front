169492857fbe6f1d2999d71a50bd34f6204735f3
soonki-98 <alstnsrl98@gmail.com>
2025-10-25 23:09:24 +0900

    feat: 닉네임 리스트 조회 기능 추가 #148

--- src/apps/contents/nicknames/index.tsx
+++ src/apps/contents/nicknames/index.tsx
@@ -1,3 +1,86 @@
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate, useSearch } from '@tanstack/react-router';
+import { Typography } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import Table from 'antd/es/table';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+
+import type { NicknameSourceDto } from '@/lib/apis/_generated/quizzesGameIoBackend.schemas';
+import { nicknameQueries } from '@/shared/service/query/nicknames';
+
+const columns: ColumnsType<NicknameSourceDto> = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: '닉네임',
+    dataIndex: 'fullname',
+    key: 'fullname',
+  },
+  {
+    title: '위치',
+    dataIndex: 'sequence',
+    key: 'sequence',
+    render: (value) => value + '번 째',
+  },
+  {
+    title: '생성일',
+    dataIndex: 'createdAt',
+    key: 'createdAt',
+    render: (date) => dayjs(date).format('YYYY년 MM월 DD일'),
+  },
+  {
+    title: '수정일',
+    dataIndex: 'updatedAt',
+    key: 'updatedAt',
+    render: (date) => dayjs(date).format('YYYY년 MM월 DD일'),
+  },
+];
+
+const PAGE_SIZE = 30;
+
 export default function NicknameListPage() {
-  return <div>Nickname List Page</div>;
+  const currentPage = useSearch({ from: '/(menus)/contents/nicknames/' }).page;
+
+  const { data, isLoading } = useQuery(
+    nicknameQueries.getList({
+      perPage: PAGE_SIZE,
+      page: currentPage,
+    })
+  );
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const navigate = useNavigate({ from: '/contents/nicknames' });
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Typography.Title level={2}>닉네임 소스 관리</Typography.Title>
+
+      <Table
+        rowSelection={{
+          type: 'checkbox',
+          selectedRowKeys,
+          onChange: setSelectedRowKeys,
+        }}
+        loading={isLoading}
+        columns={columns}
+        dataSource={data?.data}
+        scroll={{ y: '70vh' }}
+        pagination={{
+          pageSize: PAGE_SIZE,
+          current: currentPage,
+          total: data?.totalCount,
+          showQuickJumper: true,
+          showSizeChanger: false,
+          showTotal: (total, range) =>
+            `총 ${total.toLocaleString()}개 중 ${range[0]}-${range[1]}`,
+          onChange(page) {
+            navigate({ search: { page } });
+          },
+        }}
+      />
+    </div>
+  );
 }
