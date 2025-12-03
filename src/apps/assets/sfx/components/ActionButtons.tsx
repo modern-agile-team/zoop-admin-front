3491bbec65c600d0f68eb825577bd9037011b59f
zzzryt <jinjinstar3@gmail.com>
2025-12-03 13:21:45 +0900

    feat: 효과음 제거 로직 추가 #190

--- src/apps/assets/sfx/components/ActionButtons.tsx
+++ src/apps/assets/sfx/components/ActionButtons.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { App, Button } from 'antd';
+import { Alert, App, Button } from 'antd';
 import { overlay } from 'overlay-kit';
 
 import { sfxQueries } from '@/shared/service/query/sfx';
@@ -12,9 +12,9 @@ interface Props {
   onRemoveSfxs: () => void;
 }
 
-export default function ActionButtons({ selectedSfxIds }: Props) {
+export default function ActionButtons({ selectedSfxIds, onRemoveSfxs }: Props) {
   const queryClient = useQueryClient();
-  const { message } = App.useApp();
+  const { modal, message } = App.useApp();
 
   const { mutate: uploadSfx, isPending: isUploading } = useMutation({
     ...sfxQueries.upload,
@@ -30,7 +30,37 @@ export default function ActionButtons({ selectedSfxIds }: Props) {
     retry: false,
   });
 
-  const handleRemoveSfxs = async () => {};
+  const { mutateAsync: removeSfx } = useMutation({
+    ...sfxQueries.remove,
+    retry: false,
+  });
+
+  const handleRemoveSfxs = async () => {
+    const shouldRemove = await modal.confirm({
+      title: '효과음 삭제',
+      okText: '삭제',
+      cancelText: '취소',
+      okButtonProps: { danger: true },
+      icon: null,
+      content: (
+        <Alert
+          message="선택한 효과음을 삭제하시겠습니까?"
+          description="삭제된 효과음은 복구할 수 없습니다."
+          type="error"
+        />
+      ),
+    });
+
+    if (shouldRemove) {
+      try {
+        await Promise.all(selectedSfxIds.map((id) => removeSfx(id.toString())));
+        message.success('음향 효과를 삭제했어요.');
+      } catch {
+        message.error('삭제 중 오류가 발생했어요.');
+      }
+      onRemoveSfxs();
+    }
+  };
 
   return (
     <div className="flex gap-4 justify-end">
--- src/shared/service/query/sfx.ts
+++ src/shared/service/query/sfx.ts
@@ -2,6 +2,7 @@ import { mutationOptions, queryOptions } from '@tanstack/react-query';
 
 import {
   createSoundEffectControllerCreateSoundEffectAdmin,
+  deleteSoundEffectControllerDeleteSoundEffectAdmin,
   listSoundEffectsControllerListSoundEffectsAdmin,
 } from '@/lib/admins/_generated/quizzesGameIoBackend';
 import type { ListSoundEffectsControllerListSoundEffectsAdminParams } from '@/lib/admins/_generated/quizzesGameIoBackend.schemas';
@@ -14,5 +15,8 @@ export const sfxQueries = {
     }),
   upload: mutationOptions({
     mutationFn: createSoundEffectControllerCreateSoundEffectAdmin,
+  }),
+  remove: mutationOptions({
+    mutationFn: deleteSoundEffectControllerDeleteSoundEffectAdmin,
   }),
 };
